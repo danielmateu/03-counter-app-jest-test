89bb0b104585d8b330f137e691bbb575b8dd664a
Daniel Mateu <danielmateu86@gmail.com>
2023-02-21 13:59:06 +0100

    js test done

--- test/11-async-await.test.js
+++ test/11-async-await.test.js
@@ -4,11 +4,21 @@ import { getImagen } from "../src/base-pruebas/11-async-await";
 describe('Pruebas con async-await y Fetch', () => {
 
 
-    test('Debe de retornar el url de la imagen', async() => {
-        
+    test('Debe de retornar el url de la imagen', async () => {
+
         const url = await getImagen();
-        expect( typeof url ).toBe( 'string' );
+        expect(typeof url).toBe('string');
 
     })
+
+    //Debe retornar un error si no tenemos apikey
+    test('Debe retornar un error si no tenemos apikey', async () => {
+
+        const resp = await getImagen();
+        expect(resp).toBe('No se encontró la imagen');
+
+    })
+
+
 
 })
